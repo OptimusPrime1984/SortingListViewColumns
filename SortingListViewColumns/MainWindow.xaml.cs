dec749f114e08192423d489f7ae15e996e2532b3
Kan Huang <kan.huang@gmail.com>
2017-03-07 12:21:50 -0800

    Homework 3 completed

--- SortingListViewColumns/MainWindow.xaml.cs
+++ SortingListViewColumns/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,49 @@ namespace SortingListViewColumns
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //initialize the instance of list of Users
+            var users = new List<User>();
+            users.Add(new User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new User { Name = "Lisa", Password = "3LisaPwd" });
+            uxList.ItemsSource = users;            
+        }
+
+        private void GridViewColumnHeader_Name_Click(object sender, RoutedEventArgs e)
+        {
+            Sort(sender, e);
+        }
+
+        private void GridViewColumnHeader_Password_Click(object sender, RoutedEventArgs e)
+        {
+            Sort(sender, e);
+        }
+
+        private void Sort(object sender, RoutedEventArgs e)
+        {
+            GridViewColumnHeader column = (sender as GridViewColumnHeader);
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                uxList.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            uxList.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
         }
     }
 }
